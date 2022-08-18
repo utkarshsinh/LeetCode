01861245e12ebafaac699176a40a740f48a76429
utkarshsinh <107430204+utkarshsinh@users.noreply.github.com>
2022-08-18 16:55:11 +0530

    Time: 419 ms (46.80%), Space: 71.3 MB (43.85%) - LeetHub

---
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+          multimap<int, int> map;
+          for (int i = 0; i < points.size(); ++i)
+                map.insert({ points[i][0] * points[i][0] + points[i][1] * points[i][1], i });
+          vector<vector<int>> res;
+          for (auto it = map.begin(); k > 0; ++it, k--) 
+              res.push_back(points[it->second]);
+          return res;
+    }
+};
