d08d5b4be57539b7fc42adc9c8515432db5e56c0
utkarshsinh <107430204+utkarshsinh@users.noreply.github.com>
2022-08-18 16:58:12 +0530

    Time: 418 ms (47.05%), Space: 71.2 MB (44.41%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -5,7 +5,7 @@ public:
           for (int i = 0; i < points.size(); ++i)
                 map.insert({ points[i][0] * points[i][0] + points[i][1] * points[i][1], i });
           vector<vector<int>> res;
-          for (auto it = map.begin(); k > 0; ++it, k--) 
+          for (auto it = map.begin(); k > 0; it++, k--) 
               res.push_back(points[it->second]);
           return res;
     }
