8d48639962f73f5c76e8dcb110d98f262c6f16ee
utkarshsinh <107430204+utkarshsinh@users.noreply.github.com>
2022-08-19 23:33:12 +0530

    Time: 18 ms (60.74%), Space: 21.6 MB (88.99%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -16,18 +16,7 @@ public:
         if(root == NULL){
             return true;
         }
-        
-        // Condition of Binary Search Tree 
-        // left side node values of the root should be less value than the root and right side should be greater
-        // If that is true check recursivley for the root child's
-        
-        if((root->val < high) && (root->val > low)){
-          return (helper(root->left, low, root->val) && helper(root->right, root->val, high));
-
-        }else{
-            //if this root node violates the min/max constraint
-            return false;
-        }
+          return ((root->val < high) && (root->val > low)&&helper(root->left, low, root->val) && helper(root->right, root->val, high));
         
     }
     
