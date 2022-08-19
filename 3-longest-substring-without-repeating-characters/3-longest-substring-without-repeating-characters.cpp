4f375d01f17558eda8e1b213d569cc1571fc3f62
utkarshsinh <107430204+utkarshsinh@users.noreply.github.com>
2022-08-19 14:13:10 +0530

    Time: 4 ms (96.77%), Space: 8.4 MB (59.57%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,14 +1,32 @@
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-        int i=0,res=0;
-        vector<int> lastindex(256,INT_MIN);
-        for(int j=0;j<s.size();j++)
-        {
-            i=max(i,lastindex[s[j]]+1);
-                res=max(res,j-i+1);
-            lastindex[s[j]]=j;
-        }
-        return res;
+    int lengthOfLongestSubstring(string str) {
+        int n = str.size();
+ 
+    int res = 0; // result
+ 
+    // last index of all characters is initialized
+    // as -1
+    vector<int> lastIndex(256, -1);
+ 
+    // Initialize start of current window
+    int i = 0;
+ 
+    // Move end of current window
+    for (int j = 0; j < n; j++) {
+ 
+        // Find the last index of str[j]
+        // Update i (starting index of current window)
+        // as maximum of current value of i and last
+        // index plus 1
+        i = max(i, lastIndex[str[j]] + 1);
+ 
+        // Update result if we get a larger window
+        res = max(res, j - i + 1);
+ 
+        // Update last index of j.
+        lastIndex[str[j]] = j;
+    }
+    return res;
     }
 };
