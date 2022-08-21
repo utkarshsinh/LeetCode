be8a06ba85aef28b2182019380a4f077a4381375
utkarshsinh <107430204+utkarshsinh@users.noreply.github.com>
2022-08-21 11:16:14 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (67.89%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -4,17 +4,22 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        int l = 0;
-        int h = n;
-        while(l<h)
+         long int low = 1;
+        long int high = n;
+        long int mid;
+        long int result = n;
+        
+        while(low<=high)
         {
-            int mid = l + (h -l)/2 ;
+            mid = (low+high)/2;
             if(isBadVersion(mid))
             {
-                h=mid;
+                result = mid;
+                high = mid-1;
             }
-           else l=mid+1;
+            else
+                low = mid+1;
         }
-        return l;
+        return result; 
     }
 };
